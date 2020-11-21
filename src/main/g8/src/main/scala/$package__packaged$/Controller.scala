e94ce14449499ea705334d31d161d354e49cc29c
Donat Fetisov <dlfetisov@beeline.ru>
2020-11-21 05:22:16 +0300

    шаманство с размещением файла примера

--- src/main/g8/src/main/scala/$package__packaged$/Controller.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Controller.scala
@@ -1,7 +1,7 @@
 package ru.beeline.spark.etl.project
 
 object Controller extends App with SparkSessionWrapper {
-  val df1 = extractors.readCsv("data/titanic.csv")
+  val df1 = extractors.readCsv("src/main/resources/titanic.csv")
   df1.show(numRows = 20, truncate = false)
   df1.printSchema()
 }
