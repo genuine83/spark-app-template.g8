2019eb6bf86b8a215165849338aae831825c0320
Donat Fetisov <dlfetisov@beeline.ru>
2020-11-25 08:36:21 +0300

    почистил Main метод

--- src/main/g8/src/main/scala/$package__packaged$/Controller.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Controller.scala
@@ -1,7 +1,10 @@
 package ru.beeline.spark.etl.project
 
+import org.apache.spark
+
 object Controller extends App with SparkSessionWrapper {
-  val df1 = extractors.readCsv("src/main/resources/titanic.csv")
-  df1.show(numRows = 20, truncate = false)
-  df1.printSchema()
+  System.out.println("Hello Spark!")
+//  val df1 = spark.Seq(("John", "Daw")("Smith", "Brown"))
+//  df1.show(numRows = 20, truncate = false)
+//  df1.printSchema()
 }
